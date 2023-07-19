1a83bf47a3ed52e258aeccc79e63dfec234615bd
Tongxuan Wang <tw2484@nyu.edu>
2023-07-18 23:36:47 -0700

    create post

--- src/main/java/com/example/postandreplyservice/service/remote/FileService.java
+++ src/main/java/com/example/postandreplyservice/service/remote/FileService.java
@@ -3,16 +3,13 @@ package com.example.postandreplyservice.service.remote;
 import com.example.postandreplyservice.dto.FileUrlResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
 @Service
 @FeignClient("file-service")
 public interface FileService {
     @PostMapping(value = "file-service/files", consumes = "multipart/form-data")
-    ResponseEntity<FileUrlResponse> uploadFiles(@RequestParam("files") MultipartFile[] files);
+    ResponseEntity<FileUrlResponse> uploadFiles(@RequestPart("files") MultipartFile[] files);
 }
